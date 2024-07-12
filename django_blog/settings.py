76566314e2adf4b17cb491a59782ceb63bea27b9
ethan <kkjuan@msn.com>
2024-07-12 09:49:59 +0800

    modified settings.py and requirements

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -130,3 +130,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+##b kk
+## Secure cross policy
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
